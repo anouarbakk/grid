de237f0158acc7f2fecf178763ec066d3dba7533
anouarbakk <baccoucheanouar2@gmail.com>
2023-12-12 23:46:47 +0100

    this is the first version of the work

---
+++ script.js
@@ -0,0 +1,6 @@
+let container=document.querySelector("#container");
+for (let i=0;i<256;i++){
+    let grid=document.createElement("div");
+    grid.classList.add("grid");
+    container.appendChild(grid);
+}
