a6f4473cf90c17805caa93baf4db3a59f1603a90
anouarbakk <baccoucheanouar2@gmail.com>
2023-12-13 00:09:16 +0100

    i changed it so that the color of the grid change when the mouse hover over it

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let container=document.querySelector("#container");
-for (let i=0;i<256;i++){
-    let grid=document.createElement("div");
+let container = document.querySelector("#container");
+
+for (let i = 0; i < 256; i++) {
+    let grid = document.createElement("div");
     grid.classList.add("grid");
     container.appendChild(grid);
+    grid.addEventListener("mouseover", () => changeColor(grid));
+}
+
+function changeColor(grid) {
+    grid.classList.add("changeColor");
 }
